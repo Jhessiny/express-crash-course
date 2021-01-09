04e6b0d0c6f2092bf3d4b813c39c05bdfeab85f7
jhessiny <jhessinymattos@gmail.com>
2021-01-09 14:47:01 -0300

    CRU

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "john@gmail.com",
+    status: "active",
+  },
+  {
+    id: 2,
+    name: "Bob Willians",
+    email: "bob@gmail.com",
+    status: "inactive",
+  },
+  {
+    id: 3,
+    name: "Elias Fernandes",
+    email: "elias@gmail.com",
+    status: "active",
+  },
+];
+
+module.exports = members;
